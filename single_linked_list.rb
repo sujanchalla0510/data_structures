1f7409371fee62f771d7043faa80b8e8310ad1cc
sujan <schalla@healthright.com>
2018-04-12 23:01:56 -0400

    reverse single linked list and implement delete node func

--- single_linked_list.rb
+++ single_linked_list.rb
@@ -45,17 +45,67 @@ class SingleLinkedList
       @head = @head.next_node
       current_node.value
     else
-      raise "list is empty"
+      return "list is empty"
     end
   end
 
   def content
     current_node = @head
-    raise "list is empty" if current_node == nil
-    while  current_node != nil
+    return "list is empty" if current_node == nil
+    while(current_node != nil)
       print "#{current_node.value}->"
       current_node = current_node.next_node
     end
     print "\n"
+  end
+
+  def clear
+    @head = @tail = nil
+  end
+
+  def reverse
+    previous = nil
+    current_node = @head
+    while(current_node != nil)
+      next_node = current_node.next_node
+      current_node.next_node = previous
+      previous = current_node
+      current_node = next_node
+    end
+    @head = previous
+  end
+
+  def delete(value)
+    current_node = @head
+    temp = nil
+    previous = nil
+    while(current_node != nil)
+      if current_node.value == value
+        temp = current_node.next_node
+        previous.next_node = temp
+        return true
+      else
+        previous = current_node
+        current_node = current_node.next_node
+      end
+    end
+    return false
+  end
+
+  def delete_at(position)
+    current_node = @head
+    previous = nil
+    if pos == 0
+      temp = @head.next_node
+      @head = temp
+    else
+      (0...position).each do
+        return false if current_node.nil?
+        previous = current_node
+        current_node = current_node.next_node
+      end
+      previous.next_node = current_node.next_node
+    end
+    return true
   end
 end
